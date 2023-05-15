083a48c8c7f8bd7acf5d10bfa69754719ae22ea6
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2023-05-15 22:37:11 +0300

    Протестирован long-polling

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import time
 import requests
+import time
 from environs import Env
 from pprint import pprint
 
 
 DVMN_REVIEWS_URL = 'https://dvmn.org/api/user_reviews/'
+DVMN_LONG_POLLING_URL = 'https://dvmn.org/api/long_polling/'
 
 env=Env()
 env.read_env()
@@ -13,11 +16,12 @@ DVMN_PERSONAL_TOKEN = env.str('DVMN_PERSONAL_TOKEN')
 headers= {
     'Authorization': f'Token {DVMN_PERSONAL_TOKEN}'
 }
-
-response = requests.get(DVMN_REVIEWS_URL, headers=headers)
+start_time = time.monotonic()
+response = requests.get(DVMN_LONG_POLLING_URL, headers=headers)
 response.raise_for_status()
 
 review_response = response.json()
 
 pprint(review_response)
+print(f'Таймаут = {time.monotonic() - start_time}') #90 sec
 
